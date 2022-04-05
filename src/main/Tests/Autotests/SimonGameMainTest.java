b6837c5ef5529948149ef18a5b5c285a4c4db7a9
Anon4ig_Plovecsky <86836359+Anon4ig-Plovecsky@users.noreply.github.com>
2022-04-06 01:32:38 +0600

    First success

--- src/main/Tests/Autotests/SimonGameMainTest.java
+++ src/main/Tests/Autotests/SimonGameMainTest.java
@@ -1,25 +1,22 @@
 package Autotests;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.Pane;
-import org.testfx.framework.junit5.ApplicationTest;
-import static org.testfx.api.FxAssert.verifyThat;
 import com.example.simongame.MainApplication;
-import org.testfx.matcher.base.NodeMatchers;
+import org.loadui.testfx.GuiTest;
+import org.testfx.framework.junit5.ApplicationTest;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.input.MouseButton;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import org.testfx.api.FxToolkit;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
-
-import java.io.IOException;
-import java.util.Objects;
 
 public class SimonGameMainTest extends ApplicationTest {
     private final String KEY_NEW_GAME_BUTTON = "#newGameButton";
@@ -33,23 +30,29 @@ public class SimonGameMainTest extends ApplicationTest {
     Pane root;
     Stage stageMain;
     @Override
-    public void start(Stage stage) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainView.fxml")));
-        stageMain = stage;
-        stage.setScene(new Scene(root));
-        stage.setMaximized(true);
-        stage.setFullScreen(true);
-        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
-        stage.setFullScreenExitHint("");
-        stage.setResizable(false);
-        stage.show();
-        stage.toFront();
+    public void start(Stage stage) throws Exception {
+        super.start(stage);
+//        URL filePath = Paths.get("src/main/java/resources/com/example/simongame/MainView.fxml").toAbsolutePath().toUri().toURL();
+//        Parent root = FXMLLoader.load(Objects.requireNonNull(filePath));
+
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainView.fxml"));
+//        root = fxmlLoader.load();
+//        stageMain = stage;
+//        stage.setScene(new Scene(root));
+//        stage.setMaximized(true);
+//        stage.setFullScreen(true);
+//        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+//        stage.setFullScreenExitHint("");
+//        stage.setResizable(false);
+//        stage.show();
+//        stage.toFront();
     }
     @Before
     public void setUp() throws Exception {
-//        ApplicationTest.launch(MainApplication.class);
-        Stage stage = stageMain;
-            start(stage);
+        ApplicationTest.launch(MainApplication.class);
+
+//        Stage stage = stageMain;
+//            start(stage);
     }
     @After
     public void afterEachTest() throws Exception {
@@ -58,18 +61,16 @@ public class SimonGameMainTest extends ApplicationTest {
         release(new MouseButton[]{});
     }
     @Test
-    public void goToGameScene() {
+    public void goToGameScene() throws Exception {
         dropTo(KEY_NEW_GAME_BUTTON);
         clickOn(KEY_NEW_GAME_BUTTON);
         dropTo(KEY_CENTER_BUTTON);
         clickOn(KEY_CENTER_BUTTON);
-    }
-    @Test
-    public void setRecord() throws Exception {
         for(int i = 0; i < 5; i++) {
-            Thread.sleep(2000 * (i + 10));
-            @SuppressWarnings("unchecked")
-            Label stringSubsequence = (Label) root.lookup(KEY_TRUE_SUBSEQUENCE);
+            Thread.sleep(2000 * (i + 1));
+            Label stringSubsequence = (Label) find(KEY_TRUE_SUBSEQUENCE);
+//            @SuppressWarnings("unchecked")
+//            Label stringSubsequence = (Label) root.lookup(KEY_TRUE_SUBSEQUENCE);
             String[] trueSubsequence = stringSubsequence.getText().split(" ");
             for(String button : trueSubsequence)
                 pressXButton(Integer.parseInt(button));
@@ -79,6 +80,10 @@ public class SimonGameMainTest extends ApplicationTest {
         }
         dropTo(KEY_SAVE_BUTTON);
         clickOn(KEY_SAVE_BUTTON);
+    }
+    @Test
+    public void setRecord() throws Exception {
+
 
     }
     private void pressXButton(int index) throws Exception {
@@ -90,8 +95,11 @@ public class SimonGameMainTest extends ApplicationTest {
             case 3 -> idButton = KEY_BUTTON_THREE;
         }
         dropTo(idButton);
-        clickOn(idButton);
+        press(MouseButton.PRIMARY);
         Thread.sleep(100);
+        release(MouseButton.PRIMARY);
+//        clickOn(idButton);
+//        Thread.sleep(100);
     }
     //TODO: ↓
     public <T extends Node> T find(final String query) {
